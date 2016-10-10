b23af4df1e5a82c38ffa956114adfeca330afc9b
Robert <Robert@ROBERTTABLET>
2016-10-09 23:33:14 -0400

    Updated Player.cs

--- TileEngine/Player.cs
+++ TileEngine/Player.cs
@@ -9,7 +9,28 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TileEngine
 {
-    class Player
+    public class Player: Entity
     {
+        const int DOWN = 0;
+        const int LEFT = 1;
+        const int UP = 2;
+        const int RIGHT = 3;
+
+        public bool isAttacking;
+        public bool isWalking;
+        public int heading;
+        const int animationRate = 250;
+        int currentFrame;
+        long animationCounter;
+
+        int numberOfFramesInAnimation;
+        int level;
+
+        int speed;
+        Vector3 possibleUpdate;
+
+        Vector4 srcRect;
+        int roomX, roomY;
+
     }
 }
